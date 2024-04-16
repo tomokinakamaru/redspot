7f7590d2a0df055a1e902f74716f5d923c3cfb14
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2024-04-16 13:40:58 +0900

    Fix names of generated files

--- redspot/dataloader.py
+++ redspot/dataloader.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import datetime
 
 from redspot import database
 from redspot.notebook import Notebook
@@ -20,8 +21,9 @@ def _load_notebooks():
 
 def _load_panel_roots():
     panel_roots, session_roots = {}, {}
-    for _, panel, kind, args in database.get():
+    for panel_id, panel, kind, args in database.get():
         session = args.get("val")
+        panel_id = _create_panel_id(panel_id, panel)
         if kind == "ISessionContext.sessionChanged" and session:
             if panel in panel_roots:
                 if session in session_roots:
@@ -32,7 +34,13 @@ def _load_panel_roots():
                 if session in session_roots:
                     panel_roots[panel] = session_roots[session]
                 else:
-                    panel_roots[panel] = session_roots[session] = panel
+                    panel_roots[panel] = session_roots[session] = panel_id
         else:
-            panel_roots.setdefault(panel, panel)
+            panel_roots.setdefault(panel, panel_id)
     return panel_roots
+
+
+def _create_panel_id(time, panel):
+    p = "".join(panel.split("-")[1:])
+    s = datetime.fromtimestamp(time / 1000)
+    return datetime.strftime(s, f"%Y-%m%d-%H%M-{p}")
